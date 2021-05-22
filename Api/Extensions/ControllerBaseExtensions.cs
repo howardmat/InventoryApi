c452fa9ec04aa924b1df16f68c6e40903957b36e
Mat Howard <howard.mat@gmail.com>
2021-05-22 17:02:17 -0400

    Initial commit

---
+++ Api/Extensions/ControllerBaseExtensions.cs
@@ -0,0 +1,74 @@
+﻿using Api.Enums;
+using Api.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.Extensions
+{
+    public static class ControllerBaseExtensions
+    {
+        public static ActionResult GetResultFromServiceResponse(this ControllerBase controller, ServiceResponse response)
+        {
+            ActionResult result;
+
+            // Handle the status result
+            var responseStatus = response.GetStatus();
+            switch (responseStatus)
+            {
+                case ServiceResponseStatus.Success:
+                    result = controller.NoContent();
+                    break;
+                case ServiceResponseStatus.NotFound:
+                    result = controller.NotFound(response.GetErrorJson());
+                    break;
+                case ServiceResponseStatus.Error:
+                    result = controller.BadRequest(response.GetErrorJson());
+                    break;
+                case ServiceResponseStatus.Exception:
+                    result = controller.StatusCode(StatusCodes.Status500InternalServerError, response.GetErrorJson());
+                    break;
+                default:
+                    result = controller.StatusCode(StatusCodes.Status500InternalServerError);
+                    break;
+            }
+
+            return result;
+        }
+
+        public static ActionResult GetResultFromServiceResponse<T>(this ControllerBase controller, ServiceResponse<T> response, string uri = null)
+        {
+            ActionResult result;
+
+            // Handle the generic result
+            var responseStatus = response.GetStatus();
+            if (responseStatus == ServiceResponseStatus.Success)
+            {
+                if (response.Data != null)
+                {
+                    if (!string.IsNullOrEmpty(uri))
+                    {
+                        result = controller.Created(uri, response.Data);
+                    }
+                    else
+                    {
+                        result = controller.Ok(response.Data);
+                    }
+                }
+                else
+                {
+                    result = controller.NoContent();
+                }
+            }
+            else
+            {
+                result = controller.GetResultFromServiceResponse(response.ToNonGeneric());
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Api/Models/ServiceResponse.cs
@@ -0,0 +1,69 @@
+﻿using Api.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Api.Models
+{
+    public class ServiceResponse
+    {
+        private ServiceResponseStatus _status = ServiceResponseStatus.Success;
+        private ICollection<string> _errors = new HashSet<string>();
+
+        public ServiceResponse() { }
+
+        public ServiceResponse(ICollection<string> errors)
+        {
+            _errors = errors;
+        }
+
+        public ServiceResponse(ServiceResponseStatus status)
+        {
+            _status = status;
+        }
+
+        public ServiceResponse(ServiceResponseStatus status, ICollection<string> errors)
+        {
+            _status = status;
+            _errors = errors;
+        }
+
+        public ServiceResponseStatus GetStatus() => _status;
+        public ServiceResponseStatus SetError() => _status = ServiceResponseStatus.Error;
+        public ServiceResponseStatus SetNotFound() => _status = ServiceResponseStatus.NotFound;
+        public ServiceResponseStatus SetException() => _status = ServiceResponseStatus.Exception;
+
+        public string[] GetErrors()
+        {
+            return _errors.ToArray();
+        }
+
+        public string GetErrorJson()
+        {
+            return JsonSerializer.Serialize(_errors);
+        }
+
+        public void AddError(string error)
+        {
+            _errors.Add(error);
+        }
+
+        public void AddErrors(IEnumerable<string> errors)
+        {
+            foreach (var error in errors)
+                _errors.Add(error);
+        }
+    }
+
+    public class ServiceResponse<T> : ServiceResponse
+    {
+        public T Data { get; set; }
+
+        public ServiceResponse ToNonGeneric()
+        {
+            return new ServiceResponse(GetStatus(), GetErrors());
+        }
+    }
+}
