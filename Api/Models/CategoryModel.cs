f3514555e302e7fa58beee1ed19f9f68b42ee030
Mat Howard <howard.mat@gmail.com>
2021-05-23 09:48:22 -0400

    #111 Cleaning up name of Id property

--- Api/Models/CategoryModel.cs
+++ Api/Models/CategoryModel.cs
@@ -4,7 +4,7 @@ namespace Api.Models
 {
     public class CategoryModel
     {
-        public int? CategoryId { get; set; }
+        public int? Id { get; set; }
 
         [Required]
         [MaxLength(256)]
